e14ce70964beac6a137f97e7f13bb4223af07b28
D-Ram1 <104790915+D-Ram1@users.noreply.github.com>
2022-06-21 17:18:59 -0700

    updated coding on register.js

--- controllers/register.js
+++ controllers/register.js
@@ -3,13 +3,13 @@ const handleRegister = (req, res, db, bcrypt) => {
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
-	const hash = bcrypt.hashSync(password);
+  const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
       trx.insert({
         hash: hash,
         email: email
       })
-			.into('login')
+      .into('login')
       .returning('email')
       .then(loginEmail => {
         return trx('users')
@@ -23,12 +23,12 @@ const handleRegister = (req, res, db, bcrypt) => {
             res.json(user[0]);
           })
       })
-			.then(trx.commit)
+      .then(trx.commit)
       .catch(trx.rollback)
     })
     .catch(err => res.status(400).json('unable to register'))
 }
 
 module.exports = {
-	handleRegister: handleRegister
-}
+  handleRegister: handleRegister
+};
