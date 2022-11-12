cdfa7c9e36708259c4784700f1f30f7bddd9bd2d
D-Ram1 <104790915+D-Ram1@users.noreply.github.com>
2022-11-11 17:37:22 -0800

    updating coding

--- controllers/signin.js
+++ controllers/signin.js
@@ -3,7 +3,7 @@ const handleSignin = (db, bcrypt) => (req, res) => {
   if (!email || !password) {
     return res.status(400).json('incorrect form submission');
   }
-	db.select('email', 'hash').from('login')
+  db.select('email', 'hash').from('login')
     .where('email', '=', email)
     .then(data => {
       const isValid = bcrypt.compareSync(password, data[0].hash);
@@ -13,14 +13,14 @@ const handleSignin = (db, bcrypt) => (req, res) => {
           .then(user => {
             res.json(user[0])
           })
-					.catch(err => res.status(400).json('unable to get user'))
+          .catch(err => res.status(400).json('unable to get user'))
       } else {
         res.status(400).json('wrong credentials')
       }
     })
-		.catch(err => res.status(400).json('wrong credentials'))
+    .catch(err => res.status(400).json('wrong credentials'))
 }
 
 module.exports = {
-	handleSignin: handleSignin
+  handleSignin: handleSignin
 }
