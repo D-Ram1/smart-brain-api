f42911c31f0b3cfd8dfc16e05de7082b85cfddfe
D-Ram1 <104790915+D-Ram1@users.noreply.github.com>
2022-11-14 15:02:57 -0800

    updated database .env info

--- server.js
+++ server.js
@@ -24,10 +24,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection : {
-    host : '',
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME
+   connectionString : process.env.DATABASE_URL
   }
 });
 
