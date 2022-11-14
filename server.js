6dc54232e42f622621918b3f758ec1463be41a72
D-Ram1 <104790915+D-Ram1@users.noreply.github.com>
2022-11-14 14:27:23 -0800

    updated database info

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // const supabase = require('@supabase/supabase-js')
 const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
+const bodyParser = require('body-parser'); 
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
@@ -10,13 +10,24 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+// const SUPABASE_URL = 'https://poctsikqqmbbhdrizsxw.supabase.co'
+// const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvY3RzaWtxcW1iYmhkcml6c3h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgyMDUwMTksImV4cCI6MTk4Mzc4MTAxOX0.YHALq8B3hgvF1smcIsvhEsaH4A7PbIuhRYWHWilG6pA'
+// const { createClient } = supabase;
+// const supabasedb = createClient(SUPABASE_URL, SUPABASE_KEY)
+
+// async function test() {
+
+// const { data: login, error } = await supabase.from('login').select('*')
+
+// }
+
 const db = knex({
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'smart-brain'
+  connection : {
+    host : '',
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
   }
 });
 
