559791d4c2fd55121eb0676e080134a1ee7039b5
D-Ram1 <104790915+D-Ram1@users.noreply.github.com>
2024-01-18 14:42:45 -0800

    updating code for render

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
@@ -11,22 +11,28 @@ const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
-  connection : {
-   connectionString : process.env.DATABASE_URL
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB
   }
 });
 
 const app = express();
 
 app.use(cors())
-app.use(express.json()); 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.use(express.json());
+app.get('/', (req, res) => { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT || 3000, ()=> {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`App is running on port ${process.env.PORT}.`);
 })
